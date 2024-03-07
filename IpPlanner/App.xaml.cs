bf4a4029de95ea1799d9cc0ddae347fc04799c66
McAr0v <dvijkz@gmail.com>
2024-03-07 17:54:34 +0500

    Сделал навигацию по нижнему меню

--- IpPlanner/App.xaml.cs
+++ IpPlanner/App.xaml.cs
@@ -10,10 +10,50 @@ namespace IpPlanner
         {
             InitializeComponent();
 
-            
 
-            MainPage = new UserScreen();
+
+            /*var shell = new Shell();
+
+            shell.Items.Add(new Tab()
+            {
+                //FlyoutDisplayOptions = FlyoutDisplayOptions.AsMultipleItems,
+                Title = "Page One",
+                Items =
+                {
+                    new ShellContent() { Content = new RegistrationPage() }
+                }
+            });
+
+            shell.Items.Add(new Tab()
+            {
+                FlyoutDisplayOptions = FlyoutDisplayOptions.AsMultipleItems,
+                Title = "Page Two",
+                FlyoutIcon = "Icons/user-solid.svg",
+                
+                Items =
+                {
+                    new ShellContent() { Content = new UserScreen() }
+                }
+            });
+
+            shell.Items.Add(new Tab()
+            {
+                FlyoutDisplayOptions = FlyoutDisplayOptions.AsMultipleItems,
+                Title = "Page Three",
+                Items =
+                {
+                    new ShellContent() { Content = new UserScreen() }
+                }
+            });
+
+
+
+            MainPage = shell;*/
+
+            //MainPage = new UserScreen();
             //MainPage = new RegistrationPage();
+
+            MainPage = new AppShell();
         }
     }
 }
--- IpPlanner/AppShell.xaml.cs
+++ IpPlanner/AppShell.xaml.cs
@@ -1,10 +1,84 @@
-﻿namespace IpPlanner
+﻿using IpPlanner.Design.Colors;
+using IpPlanner.Screens.UserScreen;
+
+namespace IpPlanner
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            // Добавляем вкладки
+            AddTabBar();
+
+            
+
+
+            void AddTabBar()
+            {
+                // Создаем стиль для вкладок
+                var tabStyle = new Style(typeof(Tab))
+                {
+                    ApplyToDerivedTypes = true,
+                    Setters =
+                {
+                    new Setter() { Property = TabBarBackgroundColorProperty, Value = CustomColors.Black }, // Устанавливаем цвет текста вкладок
+                    new Setter() { Property = TabBarTitleColorProperty, Value = CustomColors.White }, // Устанавливаем размер шрифта вкладок
+                    new Setter() { Property = TabBarUnselectedColorProperty, Value = CustomColors.Graphite }, // Устанавливаем размер шрифта вкладок
+                    new Setter() { Property = TabBarForegroundColorProperty, Value = CustomColors.White }, // Устанавливаем размер шрифта вкладок
+                }
+                };
+
+                // Применяем стиль вкладок
+                Resources.Add(tabStyle);
+                // Создаем вкладки и добавляем их в шелл
+                Items.Add(new Tab()
+                {
+                    FlyoutDisplayOptions = FlyoutDisplayOptions.AsMultipleItems,
+                    Title = "Заказы",
+                    Icon = "file_invoice_dollar_solid.png",
+                    Items =
+                {
+                    new ShellContent() { Content = new RegistrationPage() }
+                }
+                });
+
+                Items.Add(new Tab()
+                {
+                    FlyoutDisplayOptions = FlyoutDisplayOptions.AsMultipleItems,
+                    Title = "Задачи",
+                    Icon = "list_check_solid.png",
+                    Items =
+                {
+                    new ShellContent() { Content = new UserScreen() }
+                }
+                });
+
+                Items.Add(new Tab()
+                {
+                    FlyoutDisplayOptions = FlyoutDisplayOptions.AsMultipleItems,
+                    Title = "Статистика",
+                    Icon = "chart_simple_solid.png",
+                    Items =
+                {
+                    new ShellContent() { Content = new UserScreen() }
+                }
+                }); 
+
+                Items.Add(new Tab()
+                {
+                    FlyoutDisplayOptions = FlyoutDisplayOptions.AsMultipleItems,
+                    Title = "Профиль",
+                    Icon = "user_solid.png",
+                    Items =
+                {
+                    new ShellContent() { Content = new UserScreen() }
+                }
+                });
+
+            }
+
         }
     }
 }
